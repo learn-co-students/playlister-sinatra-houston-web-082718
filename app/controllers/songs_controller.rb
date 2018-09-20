323a4043f038c643a91a18e0e7753a3452907a29
Naeblis <johnmlowery@gmail.com>
2018-09-20 17:40:23 -0500

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,8 @@
+
+
+
+
+class SongsController < ApplicationController
+
+    
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,18 @@
 # Add seed data here. Seed your database with `rake db:seed`
+Artist.destroy_all
+Genre.destroy_all
+Song.destroy_all
+
+Genre.create(:name =>"Pop")
+Genre.create(:name =>"Rap")
+Genre.create(:name =>"Country")
+
+Artist.create(:name =>"Taylor Swift")
+Artist.create(:name =>"Neffex")
+Artist.create(:name =>"Mumford & Sons")
+
+Song.create(:name => "Dust Bowl Dances")
+Song.create(:name =>"Shake It Off")
+Song.create(:name =>"Best Of Me")
+
 
